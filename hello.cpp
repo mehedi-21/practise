6d856728e7d423b335d3fcff357c3942b39eb92a
mehedi-21 <mehedi.su.cse19@gmail.com>
2021-09-06 23:59:09 +0600

    modified the program into swap program

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world" ;
+    int x,y ;
+
+    cout << "enter the value of x : " ;
+    cin >> x ;
+
+    cout << "enter the value of y : " ;
+    cin >> y ;
+
+    cout << endl << "before swapping : x = " << x << "\ty = " << y << endl ;
+
+    x = x + y ;
+    y = x - y ;
+    x = x - y ;
+
+    cout << endl << "after swapping : x = " << x << "\t\ty = " << y << endl ;
+
+
 
 
     return 0 ;
